0ea85291bc9ed982a43aea45980db47cdd7ca3f6
Alex <alexrosenk@hotmail.se>
2024-07-26 05:39:09 +0200

    Added SplashViewModel instance to control splash screen handling.

--- app/src/main/java/se/umu/alro0113/trackandbet/MainActivity.kt
+++ app/src/main/java/se/umu/alro0113/trackandbet/MainActivity.kt
@@ -4,20 +4,33 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
+import se.umu.alro0113.trackandbet.onboarding.presentation.welcome_screen.SplashViewModel
 import se.umu.alro0113.trackandbet.ui.theme.AppTheme
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    // Hilt performs constructor injection to provide SplashViewModel, which depends on a DataStoreRepository singleton provided by the AppModule.
+    @Inject
+    lateinit var splashViewModel: SplashViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen() // .setkeeponscreencondition ?
+
+            // On app startup, display a splash screen with a default white icon
+            installSplashScreen()
+
         enableEdgeToEdge()
         setContent {
             AppTheme {
-                TrackAndBetApp()
+                // The initial state of the screen is null. We need to wait for the ViewModel to set it to either
+                // the Welcome screen or the Home screen before proceeding.
+                val screen by splashViewModel.startDestination
+                screen?.let { TrackAndBetApp(startDestination = it) }
             }
         }
     }
