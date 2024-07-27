93cb28e1667f7f1fa4df2c7690a4942e06bd61d9
Alex <alexrosenk@hotmail.se>
2024-07-27 03:53:42 +0200

    refactor: getData() now returns the full DataResponse

--- app/src/main/java/se/umu/alro0113/trackandbet/marketdata/data/repository/DetailsRepositoryImpl.kt
+++ app/src/main/java/se/umu/alro0113/trackandbet/marketdata/data/repository/DetailsRepositoryImpl.kt
@@ -5,6 +5,7 @@ import se.umu.alro0113.trackandbet.marketdata.data.mapper.toNetworkError
 import se.umu.alro0113.trackandbet.marketdata.data.remote.DetailsApi
 import se.umu.alro0113.trackandbet.marketdata.domain.model.NetworkError
 import se.umu.alro0113.trackandbet.marketdata.domain.model.Data
+import se.umu.alro0113.trackandbet.marketdata.domain.model.DataResponse
 import se.umu.alro0113.trackandbet.marketdata.domain.repository.DetailsRepository
 import javax.inject.Inject
 
@@ -13,10 +14,10 @@ class DetailsRepositoryImpl @Inject constructor(
 ): DetailsRepository {
 
     // TODO must consider how vico wants full data array, while the card only wants .data[0]
-    override suspend fun getData(): Either<NetworkError, Data> {
+    override suspend fun getData(): Either<NetworkError, DataResponse> {
         return Either.catch {
-            val eodData = detailsApi.getDataResponse().data[0] // the most recent end of day list of data
-            eodData // returning the list of details
+            val eodData = detailsApi.getDataResponse()
+            eodData
         }.mapLeft { it.toNetworkError() }
     }
 }
