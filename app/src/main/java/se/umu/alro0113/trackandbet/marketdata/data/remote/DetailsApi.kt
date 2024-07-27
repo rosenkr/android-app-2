f9e0b9de0f410e6cb044da0c563ed7404384fb26
Alex <alexrosenk@hotmail.se>
2024-07-27 03:50:28 +0200

    No changes

--- app/src/main/java/se/umu/alro0113/trackandbet/marketdata/data/remote/DetailsApi.kt
+++ app/src/main/java/se/umu/alro0113/trackandbet/marketdata/data/remote/DetailsApi.kt
@@ -4,6 +4,7 @@ import se.umu.alro0113.trackandbet.marketdata.domain.model.DataResponse
 
 // TODO aapl should be an argument, not precoded
 // TODO couldnt manage how to hide api key
+// End of day details for AAPL
 interface DetailsApi {
     @GET("eod?symbols=AAPL&access_key=5ebd15bbd5ba4ddfbfd10698ea343f13")
     suspend fun getDataResponse(): DataResponse
