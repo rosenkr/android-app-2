d13c74b12865305d5f60b5c0c0be8fdf0202dbb8
Alex <alexrosenk@hotmail.se>
2024-07-29 01:13:22 +0200

    Added comments for future reference about consistency of the projects usage of ViewModel's

--- app/src/main/java/se/umu/alro0113/trackandbet/onboarding/presentation/welcome_screen/WelcomeViewModel.kt
+++ app/src/main/java/se/umu/alro0113/trackandbet/onboarding/presentation/welcome_screen/WelcomeViewModel.kt
@@ -8,6 +8,9 @@ import kotlinx.coroutines.launch
 import se.umu.alro0113.trackandbet.onboarding.data.datastore.DataStoreRepository
 import javax.inject.Inject
 
+// Although the consistent approach of the project is to have separate State files
+// for packages with a ViewModel, note that here the state management is instead
+// delegated to the DataStore repository through the function provided below
 @HiltViewModel
 class WelcomeViewModel @Inject constructor(
     private val repository: DataStoreRepository
