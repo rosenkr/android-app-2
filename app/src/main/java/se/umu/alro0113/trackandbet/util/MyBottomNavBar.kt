376f8d641b177015381c36e32e171a99e0536fe1
Alex <alexrosenk@hotmail.se>
2024-08-06 03:57:14 +0200

    no change

--- app/src/main/java/se/umu/alro0113/trackandbet/util/MyBottomNavBar.kt
+++ app/src/main/java/se/umu/alro0113/trackandbet/util/MyBottomNavBar.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarDefaults
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
